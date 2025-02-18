02fb574459fa7035ec4f28ba4678f8e72832ede9
Paulo Antunes <Antunesp950@gmail.com>
2025-02-18 20:47:18 -0300

    usando json como banco de dados

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,16 +1,41 @@
 describe('orange hrm tests', () => {
+
+
+
+
+
+  const selectorlist = {
+    usernamefield: ':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input',
+    passwordFild: ':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input',
+    loginButton: '.oxd-button'
+  }
+
+const userData ={
+  userSucess:{
+    username:"admin",
+    password:"admin123"
+  },
+
+  userFail:{
+    username:"teste",
+    password:"teste"
+
+  }
+}
+  
+
   it('login sucess', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type("Admin")
-    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type("admin123")
-    cy.get('.oxd-button').click()
+    cy.get(selectorlist.usernamefield).type(userData.userSucess.username)
+    cy.get(selectorlist.passwordFild).type(userData.userSucess.password)
+    cy.get(selectorlist.loginButton).click()
     cy.location("pathname").should('equal','/web/index.php/dashboard/index')
     cy.get('.oxd-topbar-header-breadcrumb > .oxd-text').contains("Dashboard")
   })
   it('login fail', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type("teste")
-    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type("teste")
+    cy.get(selectorlist.usernamefield).type("teste")
+    cy.get(selectorlist.passwordFild).type("teste")
     cy.get('.oxd-button').click()
     cy.get('.oxd-alert-content')
   })
